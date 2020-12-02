ab70ac4441f035c468ceed7840e93053a2a6b49d
MaxenceGiraud <maxence9giraud@gmail.com>
2020-12-02 18:35:28 +0100

    Fix autoencoder plug method

--- mla/dl/autoencoder.py
+++ mla/dl/autoencoder.py
@@ -2,6 +2,7 @@ import numpy as np
 from .neuralnetwork import NeuralNetwork
 from .layers.loss import MAE
 from .layers.inputlayer import InputLayer
+from .layers.dense import Dense
 from copy import deepcopy,copy
 
 class Encoded:
@@ -41,15 +42,23 @@ class AutoEncoder(NeuralNetwork):
      '''
     def __init__(self,input_shape,loss=MAE()):
         self.loss = loss
-        self.encoder = [InputLayer(input_shape)]
+        self.input_shape = input_shape
+        self.clear()
+    
+    def clear(self):
+        self.encoder = [InputLayer(self.input_shape)]
         self.decoder = []
         self.encoded_layer = Encoded()
-    
+        self.decoder.append(Dense(np.array(sum(self.input_shape))))
+
+        self.encoded_layer.plug(self.encoder[-1])
+        self.decoder[-1].plug(self.encoded_layer)
+
     @property
     def output_layer(self):
-        return self.decoder[-1]
+        return self.decoder[0]
         
-    def add(self,layer):
+    def add(self,layer,encoding_layer=False):
         layer_decoder = deepcopy(layer)
 
         # Connect encoder
@@ -57,16 +66,18 @@ class AutoEncoder(NeuralNetwork):
         self.encoder.append(layer)
         self.encoded_layer.plug(self.encoder[-1])
 
-        # Connect decoder
-        if len(self.decoder) == 0 :
+        if not encoding_layer :
+            # Connect decoder
             layer_decoder.plug(self.encoded_layer)
-        else :
-            self.decoder[0].plug(self.encoded_layer)
-            for i in range(1,len(self.decoder)):
-                self.decoder[i].plug(self.decoder[i-1])
-            layer_decoder.plug(self.decoder[-1])
+            for i in range(len(self.decoder)-2,0,-1):
+                self.decoder[i].plug(self.decoder[i+1])
 
-        self.decoder.append(layer_decoder)
+            self.decoder.append(layer_decoder)
+        else :
+            self.decoder[-1].plug(self.encoded_layer)
+            for i in range(len(self.decoder)-2,0,-1):
+                self.decoder[i].plug(self.decoder[i+1])
+
         self.loss.plug(self.output_layer)
 
     
@@ -77,7 +88,7 @@ class AutoEncoder(NeuralNetwork):
         delta = self.loss.backprop(X)
         delta_loss = np.copy(delta)
 
-        for i in range(len(self.decoder)-1,0,-1):
+        for i in range(0,len(self.decoder)):
             delta = self.decoder[i].backprop(delta)
         for i in range(len(self.encoder)-1,0,-1):
             delta = self.encoder[i].backprop(delta)
@@ -99,12 +110,14 @@ class AutoEncoder(NeuralNetwork):
     
     def decode(self,X):
         self.encoded_layer.set_mode("decoding")
-        decoded =  self.decoder[-1].forward(X)
+        decoded =  self.decoder[0].forward(X)
         self.encoded_layer.set_mode("linear")
 
         return decoded
     
     def get_list_layers_todisplay(self):
         display_list = copy(self.encoder)
-        display_list.extend(self.decoder)
+        d = copy(self.decoder)
+        d.reverse()
+        display_list.extend(d)
         return display_list
