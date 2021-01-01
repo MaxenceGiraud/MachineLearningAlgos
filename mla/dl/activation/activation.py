[Fix] [DL] Fix Softmax activation layer

mla/dl/activation/activation.py
@@ -46,8 +46,8 @@ class LeakyRelu:
 
 class Softmax:
     def f(self,X):
-        exps = np.exp(X- X.max(axis=1))
-        return exps / np.sum(exps,axis=1)
+        exps = np.exp(X- X.max(axis=1).reshape(-1,1))
+        return exps / np.sum(exps,axis=1).reshape(-1,1)
     
     def deriv(self,X):
         return self.f(X) *( 1-self.f(X))